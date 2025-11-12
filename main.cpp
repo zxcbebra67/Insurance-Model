5057f364d13fa40e790f3511d9bffdfa10b20cb6
vsvsvsvsvsvs <alaverduyck@gmail.com>
2025-11-12 21:35:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,12 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
-#include "CustomersManager.h"
-#include "Insurance.cpp"
+// Порхай как бабочка, жаль что петух
+#include <QApplication>
+#include "mainwindow.h"
 
-using namespace std;
-
-int main() {
-	std::default_random_engine gen;
-	std::random_device rd;
-	gen.seed(rd());
-	double Capital = 10000;
-	Life life(1000, 12, 24, 5000, 500);
-	Estate estate(1000, 12, 24, 5000, 500);
-	Vehicle vehicle(1000, 12, 24, 5000, 500);
-	int M = 1;
-	int order = 100;
-	CustomerManager manager;
-	for(int i = 0; i < M; i++){
-		Capital *= 0.91;
-		life.new_customers(gen, order, manager);
-	}
-	cout << life.get_customers();
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowTitle("Insurance Company Simulator");
+    w.show();
+    return a.exec();
 }
